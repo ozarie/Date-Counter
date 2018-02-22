33e96ed6a3bbada4727115a0c2926cfa4bade357
Your Name <oz9080@gmail.com>
2018-02-22 11:59:30 +0200

    Label Updated.

--- Date Counter/Controller/ViewController.swift
+++ Date Counter/Controller/ViewController.swift
@@ -31,22 +31,46 @@ class ViewController: UIViewController {
     
     
     @IBAction func calculateTimeButtonPressed(_ sender: UIButton) {
-        timeLabel.text = "How many time I'm abstinance?"
+        timeLabel.text = ""
         calculateTimeFromStartDateToCurrentDate()
     }
     
     
     func calculateTimeFromStartDateToCurrentDate(){
+        
+        var years : Int = 0
+        var months : Int = 0
+        var days : Int = 0
+
+        //TODO: - Fix bug that make it possible to choose a date in the future.
+        
+        //TODO: - Change the label to "year" and "years" if years < 2, For example.
+        
+        
         let timeComponents = Calendar.current.dateComponents([.year, .month, .day], from: userDate, to: Date())
-        if let years = timeComponents.year {
-            timeLabel.text?.append("\nYears: \(years),")
+        if let year = timeComponents.year {
+            years = year
         }
-        if let months = timeComponents.month {
-            timeLabel.text?.append("\nMonths: \(months)")
+        if let month = timeComponents.month {
+            months = month
         }
-        if let days = timeComponents.day {
-            timeLabel.text?.append("\nDays: \(days)")
+        if let day = timeComponents.day {
+            days = day
         }
+        
+        
+        if years == 0 {
+            timeLabel.text = "WOW! It seems that \(months) months and \(days) days passed!"
+        } else if months == 0 {
+            timeLabel.text = "WOW! It seems that \(years) years and \(days) days passed!"
+        } else if days == 0 {
+            timeLabel.text = "WOW! It seems that \(years) years and \(months) months passed!"
+        } else if years == 0 && months == 0 {
+            timeLabel.text = "WOW! It seems that \(days) days passed!"
+        } else if years == 0 && days == 0 {
+            timeLabel.text = "WOW! It seems that \(months) months passed!"
+        }
+        
     }
     
 }
