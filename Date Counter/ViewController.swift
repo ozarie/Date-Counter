2ff6406ff68588276ccaa23eee987cdf3853db57
Your Name <oz9080@gmail.com>
2018-02-22 10:49:10 +0200

    Complete calculate - with all the process until solution.

--- Date Counter/ViewController.swift
+++ Date Counter/ViewController.swift
@@ -16,29 +16,71 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var calculateTimeButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         datePicker.setDate(Date(), animated: true)
-        
+        calculateTimeButton.isEnabled = false
     }
-    
-
 
     
     @IBAction func datePickerAction(_ sender: UIDatePicker) {
-        
+
         userDate = sender.date
-
-        let theComponents = Calendar.current.dateComponents([.year, .month, .day], from: userDate, to: currentDate)
         
-        print(theComponents)
+        calculateTimeButton.isEnabled = true
 
-        timeLabel.text = theComponents.description
         
+
+//        if  {
+//            calculateTimeButton.isEnabled = true
+//        } else {
+//            calculateTimeButton.isEnabled = false
+//        }
+
+        //var years : Int = timeComponents.year!
+        //var months : Int = timeComponents.month!
+        //var days : Int = timeComponents.day!
+
+//        //print separetly
+//        print("Years: \(timeComponents.year)")
+//        print("Months: \(timeComponents.month)")
+//        print("Days: \(timeComponents.day)")
+
+        //timeLabel.text = "Years: \(years), \nMonths: \(months), \nDays: \(days)"
+
     }
+    
+    
+    
+    @IBAction func calculateTimeButtonPressed(_ sender: UIButton) {
+        timeLabel.text = "How many time I'm abstinance?"
+        calc()
+    }
+    
+    func calc(){
+        userDate = datePicker.date
+        let timeComponents = Calendar.current.dateComponents([.year, .month, .day], from: userDate, to: Date())
+        
+        //let timeComponents = Calendar.current.dateComponents([.year, .month, .day], from: userDate, to: currentDate)
+        
+        if let years = timeComponents.year {
+            timeLabel.text?.append("\nYears: \(years),")
+        }
+        if let months = timeComponents.month {
+            timeLabel.text?.append("\nMonths: \(months)")
+        }
+        if let days = timeComponents.day {
+            timeLabel.text?.append("\nDays: \(days)")
+        }
+        
+        //timeLabel.text = "How many time I'm abstinance?"
+    
+    }
+    
 }
     
     
